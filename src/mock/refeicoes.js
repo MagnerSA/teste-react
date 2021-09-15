f84e996e2ef5f29fd3ff5ccd427fb5e264c29946
Marcelo Andrade <marcelo.andrade@ccc.ufcg.edu.br>
2021-09-15 12:49:51 -0300

    update

--- src/mock/refeicoes.js
+++ src/mock/refeicoes.js
@@ -4,7 +4,7 @@ export const listaDeRefeicoes = new Map(
       id: 1,
       name: "Cereal",
       purchaseDay: "2021-09-11",
-      portion: 125,
+      portion: 125.0,
       fraction: "2/3",
       approximatePrice: 3,
     }],
@@ -13,7 +13,7 @@ export const listaDeRefeicoes = new Map(
 
       name: "Cereal",
       purchaseDay: "2021-09-11",
-      portion: 125,
+      portion: 125.0,
       fraction: "3/3",
       approximatePrice: 3,
     }],
@@ -22,7 +22,7 @@ export const listaDeRefeicoes = new Map(
 
       name: "Banana",
       purchaseDay: "2021-09-08",
-      portion: 1,
+      portion: 1.0,
       fraction: "11/12",
       approximatePrice: 0.25,
     }],
@@ -31,7 +31,7 @@ export const listaDeRefeicoes = new Map(
 
       name: "Banana",
       purchaseDay: "2021-09-08",
-      portion: 1,
+      portion: 1.0,
       fraction: "12/12",
       approximatePrice: 0.25,
     }],
@@ -40,7 +40,7 @@ export const listaDeRefeicoes = new Map(
 
       name: "Leite",
       purchaseDay: "2021-09-08",
-      portion: 250,
+      portion: 250.0,
       fraction: "4/4",
       approximatePrice: 1.25,
     }],
@@ -49,7 +49,7 @@ export const listaDeRefeicoes = new Map(
 
       name: "Aveia",
       purchaseDay: "2021-09-09",
-      portion: 33,
+      portion: 33.0,
       fraction: "5/5",
       approximatePrice: 1.6,
     }],
